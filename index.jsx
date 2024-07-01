d3acf3cbb1e6314640b3453d63945c9e10742dd1
Yusuf <bymrglmz@gmail.com>
2024-07-01 18:42:31 +0300

    determined the winner

--- index.jsx
+++ index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css';
 
 function Square({value, onClick}) {
@@ -9,6 +9,7 @@ function TicTacToe() {
 
     const [square, setSquare] = useState(Array(9).fill(''))
     const [XTurn, setXturn] = useState(true);
+    const [status, setStatus] = useState('');
 
     function getWinner(square){
         const winningPatterns = [
@@ -37,7 +38,7 @@ function TicTacToe() {
     function handleClick(getCurrentSquare){
 
         let copySquare = [...square]
-        if (copySquare[getCurrentSquare]) {
+        if (getWinner(copySquare) || copySquare[getCurrentSquare]) {
             return;
         }
         copySquare[getCurrentSquare] = XTurn ? 'X' : 'O';
@@ -46,21 +47,38 @@ function TicTacToe() {
         setSquare(copySquare)
     }
 
+    useEffect(() => {
+        if (!getWinner(square) && square.every(item => item !== '')) {
+            setSquare(`DRAW!! Restart the Game`)
+
+        }else if (getWinner(square)) {
+            setStatus(`Winner is ${getWinner(square)}`)
+        
+        }else {
+            setStatus(`Next player is ${XTurn ? 'X' : 'O'}`)
+        }
+
+    },[square,XTurn])
+
   return (
     <div className="tic-tac-toe-cont">
         {/* we need to 9 rows */}
-        <div className="row"></div>
+        <div className="row">
             <Square value={square[0]} onClick={() => handleClick(0)} />
             <Square value={square[1]} onClick={() => handleClick(1)} />
             <Square value={square[2]} onClick={() => handleClick(2)} />
-        <div className="row"></div>
+        </div>
+        <div className="row">
             <Square value={square[3]} onClick={() => handleClick(3)} />
             <Square value={square[4]} onClick={() => handleClick(4)} />
             <Square value={square[5]} onClick={() => handleClick(5)} />
-        <div className="row"></div>
+        </div>
+        <div className="row">
             <Square value={square[6]} onClick={() => handleClick(6)} />
             <Square value={square[7]} onClick={() => handleClick(7)} />
             <Square value={square[8]} onClick={() => handleClick(8)} />
+        </div>
+        <h1>{status}</h1>
     </div>
   )
 }
