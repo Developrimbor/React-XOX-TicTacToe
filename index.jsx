1ffc3ab488b582cc822f77b1b032f5a589bb90ed
Yusuf <bymrglmz@gmail.com>
2024-07-01 18:16:23 +0300

    value added

--- index.jsx
+++ index.jsx
@@ -23,17 +23,17 @@ function TicTacToe() {
     <div className="tic-tac-toe-cont">
         {/* we need to 9 rows */}
         <div className="row"></div>
-            <Square onClick={() => handleClick(0)} />
-            <Square onClick={() => handleClick(1)} />
-            <Square onClick={() => handleClick(2)} />
+            <Square value={square[0]} onClick={() => handleClick(0)} />
+            <Square value={square[1]} onClick={() => handleClick(1)} />
+            <Square value={square[2]} onClick={() => handleClick(2)} />
         <div className="row"></div>
-            <Square onClick={() => handleClick(3)} />
-            <Square onClick={() => handleClick(4)} />
-            <Square onClick={() => handleClick(5)} />
+            <Square value={square[3]} onClick={() => handleClick(3)} />
+            <Square value={square[4]} onClick={() => handleClick(4)} />
+            <Square value={square[5]} onClick={() => handleClick(5)} />
         <div className="row"></div>
-            <Square onClick={() => handleClick(6)} />
-            <Square onClick={() => handleClick(7)} />
-            <Square onClick={() => handleClick(8)} />
+            <Square value={square[6]} onClick={() => handleClick(6)} />
+            <Square value={square[7]} onClick={() => handleClick(7)} />
+            <Square value={square[8]} onClick={() => handleClick(8)} />
     </div>
   )
 }
